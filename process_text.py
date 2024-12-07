067a995227ecf4b3c21c1d139ef817068b4229c7
John Seun <iamjohnsimeon@gmail.com>
2024-12-07 23:23:32 +0100

    Set NLTK data path

--- process_text.py
+++ process_text.py
@@ -1,9 +1,13 @@
 import nltk
 from nltk.tokenize import sent_tokenize
+
+# Set NLTK data path
+nltk.data.path.append('/home/dev/nltk_data')
 nltk.download('punkt', quiet=True)
 
 def extract_sentences(text):
     return sent_tokenize(text)
+
 
 # USAGE
 # text = "Your extracted text here..."
