c5499fbf67e4dfe8477965de310583e546a3221f
John Seun <iamjohnsimeon@gmail.com>
2024-12-07 23:26:49 +0100

    Set multiple NLTK data paths

--- process_text.py
+++ process_text.py
@@ -1,13 +1,18 @@
 import nltk
+import os
 from nltk.tokenize import sent_tokenize
 
-# Set NLTK data path
-nltk.data.path.append('/home/dev/nltk_data')
-nltk.download('punkt', quiet=True)
+# Set multiple data paths
+nltk.data.path.extend([
+    '/home/dev/nltk_data',
+    '/var/www/venv/nltk_data',
+    '/usr/share/nltk_data'
+])
+
+nltk.download('punkt_tab', quiet=True)
 
 def extract_sentences(text):
     return sent_tokenize(text)
-
 
 # USAGE
 # text = "Your extracted text here..."
