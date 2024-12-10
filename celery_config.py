fa4839682a80b80c01a37ecb44e9bf38ddf33280
John Seun <iamjohnsimeon@gmail.com>
2024-12-10 16:03:18 +0100

    Initial commit

---
+++ celery_config.py
@@ -0,0 +1,17 @@
+from celery import Celery
+
+celery_app = Celery(
+    'iris',
+    broker='redis://localhost:6379/0',
+    backend='redis://localhost:6379/0'
+)
+
+celery_app.conf.update(
+    task_serializer='json',
+    result_serializer='json',
+    accept_content=['json'],
+    task_track_started=True,
+    task_time_limit=3600,
+    worker_prefetch_multiplier=1,
+    result_expires=3600
+)
