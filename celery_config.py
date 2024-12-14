cad4c4c96d83d32504455c7817098c65a269ec42
John Seun <iamjohnsimeon@gmail.com>
2024-12-14 18:58:15 +0100

    Update to include tasks module

--- celery_config.py
+++ celery_config.py
@@ -3,7 +3,8 @@ from celery import Celery
 celery_app = Celery(
     'iris',
     broker='redis://localhost:6379/0',
-    backend='redis://localhost:6379/0'
+    backend='redis://localhost:6379/0',
+    include=['tasks']
 )
 
 celery_app.conf.update(
