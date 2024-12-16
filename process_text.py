e2879851e5baf8037ed3d54cc49c386c670bd143
John Seun <iamjohnsimeon@gmail.com>
2024-12-16 23:58:03 +0100

    Debug: Enhance the process text pipeline to return meaningful sentences

--- process_text.py
+++ process_text.py
@@ -3,7 +3,6 @@ import re
 from config import Config
 from nltk.tokenize import sent_tokenize
 from nltk.corpus import stopwords
-from transformers import pipeline
 
 nltk.data.path.extend([
     '/home/dev/nltk_data',
@@ -13,31 +12,6 @@ nltk.data.path.extend([
 
 nltk.download('punkt_tab', quiet=True)
 nltk.download('stopwords', quiet=True)
-
-_paraphrase_model = None
-
-def get_paraphrase_model():
-    global _paraphrase_model
-    if _paraphrase_model is None:
-        _paraphrase_model = pipeline(
-            'text2text-generation',
-            model='t5-small',
-            device='cpu'
-        )
-    return _paraphrase_model
-
-def paraphrase_sentence(sentence):
-    model = get_paraphrase_model()
-    prefix = "paraphrase: "
-    try:
-        result = model(prefix + sentence, 
-                      max_length=100,
-                      do_sample=True,
-                      temperature=0.7,
-                      num_beams=2)
-        return result[0]['generated_text']
-    except:
-        return sentence
 
 def is_meaningful_sentence(sentence):
     words = sentence.split()
@@ -69,17 +43,11 @@ def extract_sentences(text, batch_size=1000):
         batch_text = ' '.join(batch)
         extracted = sent_tokenize(batch_text)
         
-        # meaningful_sentences = [
-        #     sent.strip() 
-        #     for sent in extracted 
-        #     if is_meaningful_sentence(sent)
-        # ]
-        meaningful_sentences = []
-        for sent in extracted:
-            if is_meaningful_sentence(sent):
-                cleaned = clean_text(sent)
-                paraphrased = paraphrase_sentence(cleaned)
-                meaningful_sentences.append(paraphrased)
+        meaningful_sentences = [
+            sent.strip() 
+            for sent in extracted 
+            if is_meaningful_sentence(sent)
+        ]
         
         sentences.extend(meaningful_sentences)
     
